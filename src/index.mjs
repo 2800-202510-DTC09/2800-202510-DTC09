fac22876a45afef82f12a2243ed949d68e8a3b66
Leonard Liu <11664880+silver886@users.noreply.github.com>
2025-04-30 16:19:37 -0700

    Remove everything

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 import {env} from 'process';
 import {join} from 'path';
 import express from 'express';
-import {connect, model, Schema} from 'mongoose';
+import {connect} from 'mongoose';
 import cors from 'cors';
 import {serve, setup} from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
@@ -46,41 +46,6 @@ app.use(
 connect(`mongodb://${env.MONGO_HOST}:${env.MONGO_PORT}/todoapp`, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
-});
-
-// Define a ToDo model
-const Todo = model(
-   'Todo',
-   new Schema({
-      text: String,
-      completed: Boolean,
-   }),
-);
-
-app.post('/todos', async (req, res) => {
-   const newTodo = new Todo({
-      text: req.body.text,
-      completed: false,
-   });
-   await newTodo.save();
-   res.status(201).json(newTodo);
-});
-
-app.get('/todos/:id', async (req, res) => {
-   const todos = await Todo.findById(req.params.id);
-   res.json(todos);
-});
-
-app.put('/todos/:id', async (req, res) => {
-   const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-   });
-   res.json(updatedTodo);
-});
-
-app.delete('/todos/:id', async (req, res) => {
-   await Todo.findByIdAndDelete(req.params.id);
-   res.status(204).end();
 });
 
 // Start the server
