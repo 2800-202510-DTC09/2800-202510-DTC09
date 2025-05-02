3aa149abca3fa4518de0f5244bcfc704cdec796e
Jago Hine <jhine3@my.bcit.ca>
2025-05-02 12:44:44 -0700

    Fix signup route conflict with dev

--- src/index.mjs
+++ src/index.mjs
@@ -92,6 +92,29 @@ app.get('/users', (req, res) => {
 });
 
 
+// Sign up flow
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.get('/signup', (req, res) => {
+	res.redirect('/signup.html');
+});
+
+// Super basic signup route. I'll add nuance/security later.
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  // Basic input check
+  if (!username || !password || password.length < 4) {
+    return res.redirect('/signup.html?error=1');
+  }
+
+  res.redirect(`/signup-success.html?username=${encodeURIComponent(username)}`);
+});
+
+
+
 app.listen(env.PORT, () => {
    console.log(`Server running on http://127.0.0.1:${env.PORT}`);
 });
