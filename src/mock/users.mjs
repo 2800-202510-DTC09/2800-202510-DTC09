9037295cfa5e644590c53678b01f75cd2584dc7c
Jago Hine <jhine3@my.bcit.ca>
2025-05-08 09:37:10 -0700

    Revise mock users to include new fields

---
+++ src/mock/users.mjs
@@ -0,0 +1,6 @@
+// Mock users for simulating backend
+export const mockUsers = [
+	{ name: 'Alice', username: 'A', email: 'alice@gmail.com', password: 'abc' },
+	{ name: 'Bob', username: 'B', email: 'bob@gmail.com', password: 'efg' }
+];
+
