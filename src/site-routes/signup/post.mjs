e79ec03db25017eb99e50c039d64a065dc372ffe
Jago Hine <jhine3@my.bcit.ca>
2025-05-09 00:05:57 -0700

    Revise login and signup to use real DB

--- src/site-routes/signup/post.mjs
+++ src/site-routes/signup/post.mjs
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
-import { mockUsers } from '../../mock/users.mjs';
+import { User } from '../../model/user.mjs'; 
+import { Error } from 'mongoose'; // 
 
-// Super basic signup route. I'll add nuance/security later.
 export async function handleSignupPost(req, res) {
 	const { username, email, password } = req.body;
 
@@ -10,40 +10,29 @@ export async function handleSignupPost(req, res) {
 		return res.redirect('/signup.html?error=values_missing');
 	}
 
-	// mocking for DB interactions
-	const existingUser = mockUsers.find(u => u.username === username);
-    	if (existingUser) {
-		return res.redirect('/signup.html?error=username_taken');
-	}
-	const existingEmail = mockUsers.find(u => u.email === email);
-    	if (existingEmail) {
-		return res.redirect('/signup.html?error=email_taken');
-	}
 	try {
 		const saltRounds = 10;
 		const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-
-		const newUser = {
-			id: "mockID", // To be replaced with Mongo ObjectId later
+		const newUser = new User({
 			username: username,
+			email: email,
 			password: hashedPassword,
-		};
+		});
 
-		// TODO: Add user to the real DB. Then redirect to /home.
+		await newUser.save();
 
-		// Ensure we've got session cookies so user can be logged in.
 		req.session.user = {
-			id: newUser.id,
+			id: newUser._id, 
 			username: newUser.username,
 			email: newUser.email,
-			};
+		};
 
-		// For testing.
-		return res.json(newUser);
+
+		res.redirect('/home.html'); 
 
 	} catch (error) {
-		return res.redirect('signup.html?error=user_creation_error');
-	}
-};
-
+		console.error('Signup error:', error);
+		return res.redirect('/signup.html?error=validation_failed');
+        }
+}
