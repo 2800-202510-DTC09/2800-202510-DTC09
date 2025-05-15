bce04c6dff4db529e5cf0a5fb01b216c358d7411
Dilinder <dgarcha9@my.bcit.ca>
2025-05-14 21:43:16 -0700

    Implament badges on profile page

--- src/api/index.mjs
+++ src/api/index.mjs
@@ -3,5 +3,4 @@ import {app} from '../index.mjs';
 
 export const api = router();
 
-
 app.use('/api', api);
---
+++ src/api/user/show-badges.mjs
@@ -0,0 +1,28 @@
+import {status} from 'http-status';
+import {User, normalize} from '../../model/user.mjs';
+
+export function registerBadgeRoute(user) {
+    user.get('/show-badges', async (req, res) => {
+        try {
+            // Check if the user is logged in
+            if (!req.session.user) {
+                return res.sendStatus(status.UNAUTHORIZED);
+            }
+            // Check if the user ID is present in the express session
+            const dbUser = await User.findById(req.session.user.id);
+            if (!dbUser) {
+                return res.sendStatus(status.NOT_FOUND);
+            }
+            // Get the badges of the user
+            const normalized = normalize(dbUser).pop();
+            normalized.badges = Array.isArray(normalized.badges)
+                ? normalized.badges.flat().filter((b) => b?.icon && b?.name)
+                : [];
+            // Return the badges
+            return res.status(status.OK).json(normalized);
+        } catch (err) {
+            console.error('Error in /show-badges route:', err);
+            return res.sendStatus(status.INTERNAL_SERVER_ERROR);
+        }
+    });
+}
