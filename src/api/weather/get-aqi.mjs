207e65ac4bcf472a9012076ac4276816df4cce26
Dilinder <dgarcha9@my.bcit.ca>
2025-05-16 13:15:14 -0700

    Add user location caching in database and add weather API endpoints

---
+++ src/api/weather/get-aqi.mjs
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import {status} from 'http-status';
+import {weather} from './index.mjs';
+
+weather.get('/air-quality', async (req, res) => {
+    const {lat, lon} = req.query;
+    if (!lat || !lon) {
+        return res
+            .status(status.BAD_REQUEST)
+            .json({error: 'Latitude and Longitude required'});
+    }
+
+    try {
+        const {OPENWEATHER_API_KEY} = process.env;
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}`,
+        );
+        return res.status(status.OK).json(response.data);
+    } catch (error) {
+        console.error('AQI fetch error:', error);
+        return res.status(status.INTERNAL_SERVER_ERROR).json({
+            error: 'Failed to fetch AQI',
+        });
+    }
+});
