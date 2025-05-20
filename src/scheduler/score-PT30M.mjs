c663784529d9c987d53560a57b4d7995442e1359
Jago Hine <jhine3@my.bcit.ca>
2025-05-20 11:20:29 -0700

    Revise score calculation

--- src/scheduler/score-PT30M.mjs
+++ src/scheduler/score-PT30M.mjs
@@ -8,44 +8,48 @@ const SCORE_LOG_FACTOR = 10;
 
 // eslint-disable-next-line import-x/no-anonymous-default-export
 export default async () => {
-    const now = Date.now();
+	const now = Date.now();
 
-    try {
-        const [users, records] = await Promise.all([
-            User.find({
-                $or: [
-                    {deletedAt: {$exists: false}},
-                    {deletedAt: null},
-                    {deletedAt: {$gt: now}},
-                ],
-            }),
-            Record.find({
-                $or: [
-                    {deletedAt: {$exists: false}},
-                    {deletedAt: null},
-                    {deletedAt: {$gt: now}},
-                ],
-            }),
-        ]);
+	try {
+		const [users, records] = await Promise.all([
+			User.find({
+				$or: [
+					{deletedAt: {$exists: false}},
+					{deletedAt: null},
+					{deletedAt: {$gt: now}},
+				],
+			}),
+			Record.find({
+				$or: [
+					{deletedAt: {$exists: false}},
+					{deletedAt: null},
+					{deletedAt: {$gt: now}},
+				],
+			}),
+		]);
 
-        await Promise.all(
-            users.map((v) => {
-                const baseScore = records.filter((w) =>
-                    w.user.equals(v.id),
-                ).length;
-                v.score = Math.round(
-                    (Math.max(
-                        Math.log10(baseScore) * SCORE_LOG_FACTOR,
-                        MIN_SCORE_PER_RECORD,
-                    ) *
-                        baseScore *
-                        toMillisecond(toSeconds(parse('P1M')))) /
-                        (now - v.createdAt),
-                );
-                return v.save();
-            }),
-        );
-    } catch (error) {
-        console.error(error);
-    }
+		await Promise.all(
+			users.map((v) => {
+				const baseScore = records.filter((w) =>
+					w.user.equals(v.id),
+				).length;
+				v.score = Math.round(
+					// Use formula only when log(baseScore) makes sens,
+					// otherwise use MIN_SCORE_PER_RECORD.
+					baseScore > 0 && (now - v.createdAt) > 0 
+					? (Math.max(
+						Math.log10(baseScore) * SCORE_LOG_FACTOR,
+						MIN_SCORE_PER_RECORD,
+					) *
+						baseScore *
+						toMillisecond(toSeconds(parse('P1M')))) /
+					(now - v.createdAt)
+					: MIN_SCORE_PER_RECORD
+				);
+				return v.save();
+			}),
+		);
+	} catch (error) {
+		console.error(error);
+	}
 };
