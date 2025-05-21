1b1bbcbae77370b931fb301ec4ad4c4d72b32a8a
Jago Hine <jhine3@my.bcit.ca>
2025-05-21 16:48:32 -0700

    Remove mock directory

--- src/mock/users.mjs
+++
@@ -1,5 +0,0 @@
-// Mock users for simulating backend
-export const mockUsers = [
-    {name: 'Alice', username: 'A', email: 'alice@gmail.com', password: 'abc'},
-    {name: 'Bob', username: 'B', email: 'bob@gmail.com', password: 'efg'},
-];
