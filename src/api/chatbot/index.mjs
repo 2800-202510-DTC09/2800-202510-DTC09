bc5c69ed92166512c3c6b83f9ca868308c1e4df0
Jago Hine <jhine3@my.bcit.ca>
2025-05-20 23:22:07 -0700

    Add chatbot api index for modular routing

--- src/api/chatbot/index.mjs
+++ src/api/chatbot/index.mjs
@@ -1,5 +1,8 @@
 import {Router as router} from 'express';
 import {api} from '../index.mjs';
+import postHandler from './post.mjs'; 
 
 export const chatbot = router();
 api.use('/chatbot', chatbot);
+
+chatbot.post('/message', postHandler);
